a6324d6879104f10750ff5e84bc52201034e698a
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-11 04:30:43 +0430

    more details

--- main.go
+++ main.go
@@ -62,3 +62,8 @@ func main() {
 //	b int = 1
 //	c int = 2
 //  )
+
+//In Go, variables are explicitly declared and used by the compiler to e.g.
+//var declares 1 or more variables.we can declare multiple variables at once.
+//Go will infer the type of initialized variables Variables declared without a corresponding initialization are zero-valued. For example, the zero value for an int is 0.
+//The := syntax is shorthand for declaring and initializing a variable, e.g. for var f string = "apple" in this case.
