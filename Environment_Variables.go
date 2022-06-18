13eb6d19ccd704a5190cb86998e6239dd7b59fcf
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-18 18:24:13 +0430

    comments

--- Environment_Variables.go
+++ Environment_Variables.go
@@ -1,10 +1,19 @@
 package main
 
+// Environment variables are a universal mechanism for conveying configuration information to Unix programs.
+//  Let’s look at how to set, get, and list environment variables.
 import (
 	"fmt"
 	"os"
 	"strings"
 )
+
+// To set a key/value pair, use os.Setenv. To get a value for a key, use os.Getenv.
+// This will return an empty string if the key isn’t present in the environment.
+
+// Use os.Environ to list all key/value pairs in the environment.
+// This returns a slice of strings in the form KEY=value. You can strings.
+// SplitN them to get the key and value. Here we print all the keys.
 
 func main() {
 
@@ -18,6 +27,11 @@ func main() {
 		fmt.Println(pair[0])
 	}
 }
+
+// more:
+// Running the program shows that we pick up the value for FOO that we set in the program, but that BAR is empty.
+// The list of keys in the environment will depend on your particular machine.If we set BAR in the environment first,
+//  the running program picks that value up
 
 // output :
 // FOO: 1
